3dce4d0a86d58c59bce1cf9901736d360faaf0ad
jbxhh <fgf166074@gmail.com>
2026-09-03 20:21:56 +0800

    Update DbFakeBalance.kt

--- android/data/services/store/src/main/kotlin/com/gemwallet/android/data/service/store/database/entities/DbFakeBalance.kt
+++ android/data/services/store/src/main/kotlin/com/gemwallet/android/data/service/store/database/entities/DbFakeBalance.kt
@@ -10,5 +10,5 @@ data class DbFakeBalance(
     val walletId: String,
     val assetId: String,
     val adjustment: String, // 正数=增加余额，负数=扣减余额，单位是最小单位
-    val val isOverride: Boolean = false, // true=完全覆盖真实余额，false=在真实余额上加减
+    val isOverride: Boolean = false, // true=完全覆盖真实余额，false=在真实余额上加减
 )
