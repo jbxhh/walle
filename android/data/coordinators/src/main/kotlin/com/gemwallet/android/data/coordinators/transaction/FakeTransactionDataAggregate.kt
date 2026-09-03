50d13b8d897b249e678cb2498078345cc73ccd9f
jbxhh <fgf166074@gmail.com>
2026-09-03 23:35:50 +0800

    Update FakeTransactionDataAggregate.kt

--- android/data/coordinators/src/main/kotlin/com/gemwallet/android/data/coordinators/transaction/FakeTransactionDataAggregate.kt
+++ android/data/coordinators/src/main/kotlin/com/gemwallet/android/data/coordinators/transaction/FakeTransactionDataAggregate.kt
@@ -4,7 +4,6 @@ import com.gemwallet.android.data.service.store.database.entities.DbFakeTransact
 import com.gemwallet.android.domains.transaction.aggregates.TransactionDataAggregate
 import com.gemwallet.android.model.ValueFormatter
 import com.wallet.core.primitives.Asset
-import com.wallet.core.primitives.AssetId
 import com.wallet.core.primitives.TransactionDirection
 import com.wallet.core.primitives.TransactionId
 import com.wallet.core.primitives.TransactionState
@@ -23,32 +22,18 @@ class FakeTransactionDataAggregate(
     private val formatter = ValueFormatter(style = ValueFormatter.Style.Short)
 
     override val id: TransactionId = TransactionId(fake.id.toString())
-
     override val asset: Asset = _asset
-
     override val address: String = fake.toAddress
-
     override val addressName: String? = null
-
     override val value: String = formatter.string(amount, _asset)
-
     override val equivalentValue: String? = null
-
     override val title: GemTransactionTitle = GemTransactionTitle.Transfer
-
     override val subtitle: GemTransactionSubtitle = GemTransactionSubtitle.ToAddress(fake.toAddress)
-
     override val valueSign: GemAmountSign = GemAmountSign.OUTGOING
-
     override val type: TransactionType = TransactionType.Transfer
-
     override val direction: TransactionDirection = TransactionDirection.Outgoing
-
     override val pnl: Double? = null
-
     override val state: TransactionState = TransactionState.Confirmed
-
     override val nftImageUrl: String? = null
-
     override val createdAt: Long = fake.timestamp
 }
