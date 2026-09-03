f865153d6d21551c22ee07a3bccbb75464732c7b
jbxhh <fgf166074@gmail.com>
2026-09-03 23:42:37 +0800

    Update build.gradle.kts

--- android/features/confirm/viewmodels/build.gradle.kts
+++ android/features/confirm/viewmodels/build.gradle.kts
@@ -48,8 +48,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":data:coordinators"))
-    api(project(":ui-models"))
+    api(project(":data:coordinators"))
     implementation(project(":ui"))
     implementation(project(":data:services:gemstone"))
     implementation(project(":gemcore"))
