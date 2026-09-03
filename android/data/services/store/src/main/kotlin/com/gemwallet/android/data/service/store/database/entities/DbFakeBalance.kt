f59c23636407cfe60e0e36ee261ed5a75111a3ec
jbxhh <fgf166074@gmail.com>
2026-09-03 20:02:20 +0800

    Create DbFakeBalance.kt

---
+++ android/data/services/store/src/main/kotlin/com/gemwallet/android/data/service/store/database/entities/DbFakeBalance.kt
@@ -0,0 +1,14 @@
+package com.gemwallet.android.data.service.store.database.entities
+
+import androidx.room.Entity
+
+@Entity(
+    tableName = "fake_balances",
+    primaryKeys = ["walletId", "assetId"]
+)
+data class DbFakeBalance(
+    val walletId: String,
+    val assetId: String,
+    val adjustment: String, // 正数=增加余额，负数=扣减余额，单位是最小单位
+    val override: Boolean = false, // true=完全覆盖真实余额，false=在真实余额上加减
+)
